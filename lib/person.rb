b0f5bbc1c96988f88182183c39a39c13fd73f84f
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-18 16:01:51 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
+require 'pry'
+class Person
+
+    total = 0
+
+    attr_accessor :bank_account, :happiness
+    attr_reader :name
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+    end
+
+    def happiness=(happiness)
+        binding.pry
+        if @happiness 
+        end
+    end
+
+    
+
+
+    # def name
+    #     @name
+    # end
+
+
+end
+
+
+person = Person.new("Raul")
+binding.pry
 # your code goes here
+0
