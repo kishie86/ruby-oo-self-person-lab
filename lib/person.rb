e65774e12594bfd00cacd8eab9e7be03d62b84f3
github username <github email address>
2020-11-18 18:02:29 -0600

    why not @happiness?

--- lib/person.rb
+++ lib/person.rb
@@ -1,30 +1,48 @@
 require 'pry'
 class Person
 
-    happiness = 0
+    #happiness = 0
 
-    attr_accessor :bank_account, :happiness
+    attr_accessor :bank_account, :happiness, :hygiene
     attr_reader :name
-    def initialize(name, bank_account = 25, happiness = 8)
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
         @name = name
         @bank_account = bank_account
         @happiness = happiness
+        @hygiene = hygiene
     end
 
     def happiness=(happiness)
         #binding.pry
-        if happiness <= 10 
-            @happiness = happiness
-        elsif happiness > 10
+        if happiness > 10
             @happiness = 10
-        else 
+        elsif happiness < 0
             @happiness = 0
-            #binding.pry
+        else
+            @happiness = happiness
         end
-        #@happiness
+        
+            #binding.pry
     end
 
-    
+    def hygiene=(hygiene)
+        if hygiene > 10
+            @hygiene = 10
+        elsif hygiene < 0
+            @hygiene = 0
+        else 
+            @hygiene = hygiene
+        end
+
+    end
+
+   def happy? 
+        if happiness > 7
+            true
+        else
+           false
+        end
+    end
 
 
     # def name
