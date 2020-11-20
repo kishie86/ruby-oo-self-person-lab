d244b6dc410107847aec361d8d16bc13c552ea77
github username <github email address>
2020-11-19 20:30:01 -0600

    Pair Lab: Kishana and Kirsty

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,18 @@
 require 'pry'
 class Person
 
-    #happiness = 0
 
     attr_accessor :bank_account, :happiness, :hygiene
     attr_reader :name
-    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+    def initialize(name)
         @name = name
-        @bank_account = bank_account
-        @happiness = happiness
-        @hygiene = hygiene
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
     end
 
     def happiness=(happiness)
-        #binding.pry
+        
         if happiness > 10
             @happiness = 10
         elsif happiness < 0
@@ -22,7 +21,6 @@ class Person
             @happiness = happiness
         end
         
-            #binding.pry
     end
 
     def hygiene=(hygiene)
@@ -37,22 +35,53 @@ class Person
     end
 
    def happy? 
-        if happiness > 7
-            true
+        self.happiness > 7
+    end
+
+    def clean? 
+        self.hygiene > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            "blah blah sun blah rain"
         else
-           false
+            "blah blah blah blah blah"
         end
     end
 
-
-    # def name
-    #     @name
-    # end
-
-
 end
 
-
+person1 = Person.new("Kirsty")
+person2 = Person.new("Katia")
 #person = Person.new("Raul")
 #binding.pry
 # your code goes here
